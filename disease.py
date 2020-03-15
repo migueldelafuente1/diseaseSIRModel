fba1380c2d0db5c8458e4766b11928674e214930
miguel <migueldelafuente4@gmail.com>
2020-03-15 23:39:36 +0100

    Version 2 with iterator, still not working

--- disease.py
+++ disease.py
@@ -48,11 +48,16 @@ class DiseaseSimulation(object):
     INFECTEDS_COULD_DIE = False
     
     def __init__(self,
-                 t_step  = 0.01,
-                 days    = 100,
-                 N_population    = 1000,
-                 contagious_rate = 0.0,
-                 recovery_rate   = 0.0):
+                 t_step  = 0.01, # days
+                 days    = 100,  # days
+                 N_population    = 200000, # persons
+                 contagious_rate = 0.0,    # persons/day
+                 recovery_rate   = 0.0):   # person/day (= time to recovery**-1)
+        """ 
+        Args:
+        :contagious_rate = contacts/day * probability of transmission by contact
+        :recovery_rate = 1 / days to recovery (period of infection)
+        """
         
         self.t_step  = t_step #min(t_step, days * contagious_rate * recovery_rate / 1e5)
         self.days    = days
@@ -60,21 +65,19 @@ class DiseaseSimulation(object):
         self.N_population = N_population
         
         self.time = np.arange(.0, days, t_step)
-        self.susceptible = np.zeros(self.N_steps)
-        self.infected    = np.zeros(self.N_steps)
-        self.recovered   = np.zeros(self.N_steps)
+        self.susceptible = [N_population]
+        self.infected    = [1]
+        self.recovered   = [0]
         
-        self.d_susceptible = np.zeros(self.N_steps)
-        self.d_infected    = np.zeros(self.N_steps)
-        self.d_recovered   = np.zeros(self.N_steps)
+        self.d_susceptible = []
+        self.d_infected    = []
+        self.d_recovered   = []
         
-        self.susceptible[0] = N_population
-        self.infected[0]    = 1
-        
-        self.CONT_RATE = round(contagious_rate, 4)
+        self.CONT_RATE = round(contagious_rate/N_population, 4)
         self.RECO_RATE = round(recovery_rate, 4)
         
         self.max_infected = None
+        self.__defineDerivates()
     
     def __repr__(self):
 
@@ -101,65 +104,62 @@ class DiseaseSimulation(object):
     INFECTED    = 'infected'
     RECOVERED   = 'recovered'
     
-    DERIVATES_1st = {SUSCEPTIBLE : 'd_susceptible',
-                     INFECTED    : 'd_infected',
-                     RECOVERED   : 'd_recovered'}
-    
-    VAR_INDEX = {SUSCEPTIBLE: 0, 
-                 INFECTED   : 1,
-                 RECOVERED  : 2}
+    VARS = [SUSCEPTIBLE, INFECTED, RECOVERED]
     
     VAR_COLORS= {SUSCEPTIBLE: 'b', 
                  INFECTED   : 'r',
                  RECOVERED  : 'g'}
     
-    def getVariables(self, i):
-        return (self.susceptible[i], self.infected[i], self.recovered[i])
+    DERIVATES_1st = {SUSCEPTIBLE : 'd_susceptible',
+                     INFECTED    : 'd_infected',
+                     RECOVERED   : 'd_recovered'}
+    
+    def getVariableTuple(self, i=-1):
+        return tuple([getattr(self, var)[i] for var in self.VARS])
     
     def getResults(self):
         """ Get all the set of results for other uses"""
-        return (self.susceptible, self.infected, self.recovered)
+        return tuple([getattr(self, var) for var in self.VARS])
     
-    def _derivates(self, sus_i, inf_i, rec_i, variable=None):
-        if variable == self.SUSCEPTIBLE:
-            return -1 * self.CONT_RATE * sus_i * inf_i
+    def __defineDerivates(self):
         
-        elif variable == self.INFECTED:
-            return (self.CONT_RATE * sus_i * inf_i) - (self.RECO_RATE * inf_i)
+        self._derivates = {}
         
-        elif variable == self.RECOVERED:
-            return self.RECO_RATE * inf_i
-        
-        else:
-            raise Exception(f"Invalid variable given [{variable}]")
+        self._derivates[self.SUSCEPTIBLE]= lambda s,i,r: -self.CONT_RATE*s*i
+        self._derivates[self.INFECTED] = lambda s,i,r: (self.CONT_RATE*s*i) - (self.RECO_RATE*i)
+        self._derivates[self.RECOVERED]= lambda s,i,r: self.RECO_RATE*i
+    
+    
+    def __euler(self, i):
+        new = {}
+        for var_name, eq in self._derivates.items():
             
-    def _euler(self, *vars_, variable=None):
-        """ Euler method for ODE solving """
-        i_step = (self.t_step**2) * self._derivates(*vars_, variable)
-        val = max(min(self.N_population, 
-                      i_step + vars_[self.VAR_INDEX[variable]]), 0)
-        return val, i_step
+            new[var_name] = getattr(self, var_name)[-1]
+            step = (self.t_step**2) * eq(*self.getVariableTuple())
+            # Record the the difference
+            getattr(self, self.DERIVATES_1st[var_name]).append(step)
+            
+            new[var_name] += step
+            #new[var_name] = max(min(self.N_population, new[var_name]), 0.01)
+            
+            getattr(self, var_name).append(new[var_name])
+            # Define the maximum
+            if ((var_name == self.INFECTED) 
+                and (step < 0) and (not self.max_infected)):
+                self.max_infected = (round(self.time[i],2), round(new[var_name]))
     
     def __call__(self):
         """ run the execution for the object inputs """
         for i in range(self.N_steps-1):
-            vars_ = self.getVariables(i)
-            for _variable in self.VAR_INDEX.keys():
-                value, step = self._euler(*vars_, variable=_variable)
-                
-                if _variable == self.SUSCEPTIBLE:
-                    self.susceptible[i+1], self.d_susceptible[i] = value, step
-                elif _variable == self.INFECTED:
-                    self.infected[i+1], self.d_infected[i] = value, step
-                    if (step < 0) and (not self.max_infected):
-                        self.max_infected = (round(self.time[i],2), round(value))
-                elif _variable == self.RECOVERED:
-                    self.recovered[i+1], self.d_recovered[i] = value, step 
+            self.__euler(i)
+                        
     
     GRAPH_LABEL = 0
     @classmethod
     def graphLabelIncrement(cls):
         cls.GRAPH_LABEL += 1
+    def getDetails(self):
+        print(self)
         
     def graph(self, details=True, logY=False):
         """ Graph the results using matplotlib, also prints the object inputs"""
@@ -171,7 +171,7 @@ class DiseaseSimulation(object):
         self.graphLabelIncrement()
         
         fig, ax = plt.subplots()
-        for _var in self.VAR_INDEX.keys():
+        for _var in self.VARS:
             ax.plot(self.time, 
                     getattr(self, _var),
                     self.VAR_COLORS[_var],
@@ -211,7 +211,7 @@ detected_list = [(0,  2),    (16, 31),
 
 i_rate, inf_y_abs = getContagiousRate(detected_list)
 t = 33
-print(f"IR:{i_rate} abscI:{inf_y_abs}")
+print(f"IR: {i_rate}  abscI: {inf_y_abs}")
 _infected = round(inf_y_abs*np.exp(i_rate*t))
 print(f"{(t_0+timedelta(days=t)).date()} : infected= {_infected}")
 
@@ -223,7 +223,7 @@ if __name__ == '__main__':
     ## Let be the parameters: (contagious_rate, recovery_rate) [people/day]
     params = [(0.265, 0.05), (2.5, 10.)]
     params_r_const = [(0.1 + 0.3*r, 20.) for r in range(5)]
-    params_c_const = [(0.05, 2.1 + 4.*c) for c in range(6)]
+    params_c_const = [(0.25, 2.1 + 4.*c) for c in range(0, 6, 2)]
     params_t_var   = [(0.05, 6.0, 0.01 + 0.01*t) for t in range(0, 10, 2)]
     
     print("CR\tRR\tmax_infected")
@@ -235,4 +235,5 @@ if __name__ == '__main__':
                                )
         ds()
         ds.graph(details=False)
+#        ds.getDetails()
         print(f"{ds.CONT_RATE}\t{ds.RECO_RATE}\t{ds.max_infected}")
